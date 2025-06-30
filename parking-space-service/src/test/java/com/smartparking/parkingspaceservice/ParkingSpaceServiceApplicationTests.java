7f45a952cc5c606892db9046b48f44436a22f27c
avindutharushan <avindutharushan2002@gmail.com>
2025-06-30 09:01:38 +0530

    update the project by adding a config server

--- parking-space-service/src/test/java/com/smartparking/parkingspaceservice/ParkingSpaceServiceApplicationTests.java
+++ parking-space-service/src/test/java/com/smartparking/parkingspaceservice/ParkingSpaceServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package lk.ijse.parkingspaceservice;
+package com.smartparking.parkingspaceservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
